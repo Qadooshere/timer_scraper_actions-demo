267e22a59643d2788f455ab799052cc4a72da808
Qadoos <abdulqadoos18@gmail.com>
2024-06-02 18:39:11 +0500

    I have added second Blob Trigger and Updated the Azure Portal APP

--- function_app.py
+++ function_app.py
@@ -37,3 +37,34 @@ def timer_trigger_actions(myTimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+
+
+@app.blob_trigger(arg_name="myblob", path="instadataupload/{name}.csv",
+                  connection="instascraperstorage_STORAGE") 
+def wp_blob_trigger(myblob: func.InputStream):
+    
+    logging.info(f"Python blob trigger function processed blob"
+                 f"Name: {myblob.name}"
+                 f"Blob Size: {myblob.length} bytes")
+    
+    # Define the URLs
+    trigger_url = "https://google.com"
+    processing_url = "https://juh.wesser.info/wp-load.php?import_key=GKlhEzpN&import_id=8&action=processing"
+
+    try:
+        # Trigger the import
+        trigger_response = requests.get(trigger_url)
+        if trigger_response.status_code == 200:
+            logging.info("Blob Import trigger URL called successfully .")
+        else:
+            logging.error(f"Failed to trigger import in Blob. Status code: {trigger_response.status_code}")
+        
+        # Call the processing URL if needed
+        processing_response = requests.get(processing_url)
+        if processing_response.status_code == 200:
+            logging.info("Blob Processing URL called successfully.")
+        else:
+            logging.error(f"Failed to call processing URL inn BLOB. Status code: {processing_response.status_code}")
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
